ddaa855d637804c8f1ff6e8a4bee6648f6ab3bf5
caydenyu <caydenn.yuu@gmail.com>
2024-03-23 19:32:10 -0700

    Plot Updates

--- DESEq2/Genus_Bar_Graphs.R
+++ DESEq2/Genus_Bar_Graphs.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(phyloseq)
 library(ggplot2)
-library(ggsignif)
+
 
 ##### load phyloseq object #####
 load("../phyloseq/pd_phyloseq.RData")
@@ -44,7 +44,7 @@ df_joined$treatment = dplyr::recode(df_joined$treatment,
                                     "4" = "Pramipexole",
                                     "5" = "Rasagiline",
                                     "6" = "Amantadine")
-df_joined <- df_joined <- df_joined %>%
+df_joined <- df_joined %>%
   filter(treatment != 7) %>%
   rename(Treatment = treatment)
 
@@ -59,22 +59,24 @@ df_sumed$rel_abs = df_sumed$sum_abs/individuals_per_treatment
 
 
 #generating the plot
-ggplot(df_sumed, aes(Treatment, rel_abs, fill = Treatment)) +
-  geom_col() + # Plotting bars
-  scale_fill_manual(values = c("Non-PD" = "#104E8B", "PD-untreated" = "#256681", 
-                               "Entacapone" = "#3A7E78", "Pramipexole" = "#569866", 
-                               "Rasagiline" = "#78B24C", "Amantadine" = "#9ACD32")) + # Setting custom colors
+Bifidobacterium_bar <- ggplot(df_sumed, aes(Treatment, rel_abs, fill = Treatment)) +
+  geom_col(color = "black") + # Plotting bars
+  scale_fill_manual(values = c("Non-PD" = "sienna1", "PD-untreated" = "indianred2", 
+                               "Entacapone" = "#9ACD32", "Pramipexole" = "#569866", 
+                               "Rasagiline" = "#104E8B", "Amantadine" = "steelblue2")) + # Setting custom colors
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05))) + # Adjusting expansion of y-axis
   theme_classic() + # Changing plot theme to a white background
   theme(
-    axis.title = element_text(face = "bold", size = rel(1.5)), # Changing axis label font
-    axis.text.x = element_text(size = rel(1.5), angle = 45, hjust = 1), # Changing x axis text font
-    axis.text.y = element_text(size = rel(1.5)), # Changing y axis text font
+    axis.title = element_text(face = "bold", size = rel(2)), # Changing axis label font
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_text(color = "black", face = "bold", size = rel(2)), # Changing y axis text font
     legend.text = element_text(size = rel(1.2)), # Changing legend text font
-    legend.title = element_text(size = rel(1.5)) # Changing legend title font
+    legend.title = element_text(face = "bold", size = rel(1.5)) # Changing legend title font
   ) +
   labs(x = NULL, y = "Abundance (normalized)", title = "") +
-  ggtitle("Bifidobacterium \n Abundance") +
-  theme(plot.title = element_text(face = "bold", size = rel(1.8), hjust = 0.5)) # Adjusting title size and centering
+  ggtitle("Bifidobacterium\n Abundance") +
+  theme(plot.title = element_text(face = "bold", size = rel(2.5), hjust = 0.5)) 
 
 
 
@@ -131,24 +133,145 @@ df_sumed = df_joined %>%
 #calculating the relative abundance of the group 
 df_sumed$rel_abs = df_sumed$sum_abs/individuals_per_treatment
 
-
 #generating the plot
-ggplot(df_sumed, aes(Treatment, rel_abs, fill = Treatment)) +
-  geom_col() + # Plotting bars
-  scale_fill_manual(values = c("Non-PD" = "#104E8B", "PD-untreated" = "#256681", 
-                               "Entacapone" = "#3A7E78", "Pramipexole" = "#569866", 
-                               "Rasagiline" = "#78B24C", "Amantadine" = "#9ACD32")) + # Setting custom colors
+Prevotella_bar <- ggplot(df_sumed, aes(Treatment, rel_abs, fill = Treatment)) +
+  geom_col(color = "black") + # Plotting bars
+  scale_fill_manual(values = c("Non-PD" = "sienna1", "PD-untreated" = "indianred2", 
+                               "Entacapone" = "#9ACD32", "Pramipexole" = "#569866", 
+                               "Rasagiline" = "#104E8B", "Amantadine" = "steelblue2")) + # Setting custom colors
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05))) + # Adjusting expansion of y-axis
   theme_classic() + # Changing plot theme to a white background
   theme(
-    axis.title = element_text(face = "bold", size = rel(1.5)), # Changing axis label font
-    axis.text.x = element_text(size = rel(1.5), angle = 45, hjust = 1), # Changing x axis text font
-    axis.text.y = element_text(size = rel(1.5)), # Changing y axis text font
+    axis.title = element_text(face = "bold", size = rel(2)), # Changing axis label font
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_text(color = "black", face = "bold", size = rel(2)), # Changing y axis text font
     legend.text = element_text(size = rel(1.2)), # Changing legend text font
-    legend.title = element_text(size = rel(1.5)) # Changing legend title font
+    legend.title = element_text(face = "bold", size = rel(1.5)) # Changing legend title font
   ) +
   labs(x = NULL, y = "Abundance (normalized)", title = "") +
-  ggtitle("Prevotella \n Abundance") +
-  theme(plot.title = element_text(face = "bold", size = rel(1.8), hjust = 0.5)) # Adjusting title size and centering
+  ggtitle("Prevotella\n Abundance") +
+  theme(plot.title = element_text(face = "bold", size = rel(2.5), hjust = 0.5)) 
+
+
+####  Lachnospiraceae graph ####
+taxa = c("g__Lachnospiraceae_UCG-001")
+taxa_level = "Genus"
+sub_beta_RA <- subset_taxa(pd_phyloseq, Genus == taxa)
+sub_beta_order_RA <-tax_glom(sub_beta_RA, taxrank = taxa_level, NArm = FALSE)
+ASV_df  =  data.frame(t(otu_table(sub_beta_order_RA)))
+colnames(ASV_df) = "abs"
+ASV_df$ID = rownames(ASV_df)
+meta = data.frame(sample_data(sub_beta_order_RA))
+meta$ID = rownames(meta)
+df_joined = inner_join(meta, ASV_df , by = "ID")
+individuals_per_treatment = c(nrow(filter(df_joined,df_joined$treatment == 1)),
+                              nrow(filter(df_joined,df_joined$treatment == 2)),
+                              nrow(filter(df_joined,df_joined$treatment == 3)),
+                              nrow(filter(df_joined,df_joined$treatment == 4)),
+                              nrow(filter(df_joined,df_joined$treatment == 5)),
+                              nrow(filter(df_joined,df_joined$treatment == 6)))
+df_joined$treatment = dplyr::recode(df_joined$treatment,
+                                    "1" = "Non-PD",
+                                    "2" = "PD-untreated",
+                                    "3" = "Entacapone",
+                                    "4" = "Pramipexole",
+                                    "5" = "Rasagiline",
+                                    "6" = "Amantadine")
+df_joined <- df_joined <- df_joined %>%
+  filter(treatment != 7) %>%
+  rename(Treatment = treatment)
+df_sumed = df_joined %>%
+  group_by(Treatment) %>%
+  summarise(sum_abs = sum(abs))
+df_sumed$rel_abs = df_sumed$sum_abs/individuals_per_treatment
+Lachnospiraceae_bar <- ggplot(df_sumed, aes(Treatment, rel_abs, fill = Treatment)) +
+  geom_col(color = "black") + # Plotting bars
+  scale_fill_manual(values = c("Non-PD" = "sienna1", "PD-untreated" = "indianred2", 
+                               "Entacapone" = "#9ACD32", "Pramipexole" = "#569866", 
+                               "Rasagiline" = "#104E8B", "Amantadine" = "steelblue2")) + # Setting custom colors
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05))) + # Adjusting expansion of y-axis
+  theme_classic() + # Changing plot theme to a white background
+  theme(
+    axis.title = element_text(face = "bold", size = rel(2)), # Changing axis label font
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_text(color = "black", face = "bold", size = rel(2)), # Changing y axis text font
+    legend.text = element_text(size = rel(1.2)), # Changing legend text font
+    legend.title = element_text(face = "bold", size = rel(1.5)) # Changing legend title font
+  ) +
+  labs(x = NULL, y = "Abundance (normalized)", title = "") +
+  ggtitle("Lachnospiraceae_UCG-001\n Abundance") +
+  theme(plot.title = element_text(face = "bold", size = rel(2.5), hjust = 0.5)) 
 
 
 
+####  Akkermansia graph ####
+taxa = c("g__Akkermansia")
+taxa_level = "Genus"
+sub_beta_RA <- subset_taxa(pd_phyloseq, Genus == taxa)
+sub_beta_order_RA <-tax_glom(sub_beta_RA, taxrank = taxa_level, NArm = FALSE)
+ASV_df  =  data.frame(t(otu_table(sub_beta_order_RA)))
+colnames(ASV_df) = "abs"
+ASV_df$ID = rownames(ASV_df)
+meta = data.frame(sample_data(sub_beta_order_RA))
+meta$ID = rownames(meta)
+df_joined = inner_join(meta, ASV_df , by = "ID")
+individuals_per_treatment = c(nrow(filter(df_joined,df_joined$treatment == 1)),
+                              nrow(filter(df_joined,df_joined$treatment == 2)),
+                              nrow(filter(df_joined,df_joined$treatment == 3)),
+                              nrow(filter(df_joined,df_joined$treatment == 4)),
+                              nrow(filter(df_joined,df_joined$treatment == 5)),
+                              nrow(filter(df_joined,df_joined$treatment == 6)))
+df_joined$treatment = dplyr::recode(df_joined$treatment,
+                                    "1" = "Non-PD",
+                                    "2" = "PD-untreated",
+                                    "3" = "Entacapone",
+                                    "4" = "Pramipexole",
+                                    "5" = "Rasagiline",
+                                    "6" = "Amantadine")
+df_joined <- df_joined <- df_joined %>%
+  filter(treatment != 7) %>%
+  rename(Treatment = treatment)
+df_sumed = df_joined %>%
+  group_by(Treatment) %>%
+  summarise(sum_abs = sum(abs))
+df_sumed$rel_abs = df_sumed$sum_abs/individuals_per_treatment
+Akkermansia_bar <- ggplot(df_sumed, aes(Treatment, rel_abs, fill = Treatment)) +
+  geom_col(color = "black") + # Plotting bars
+  scale_fill_manual(values = c("Non-PD" = "sienna1", "PD-untreated" = "indianred2", 
+                               "Entacapone" = "#9ACD32", "Pramipexole" = "#569866", 
+                               "Rasagiline" = "#104E8B", "Amantadine" = "steelblue2")) + # Setting custom colors
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05))) + # Adjusting expansion of y-axis
+  theme_classic() + # Changing plot theme to a white background
+  theme(
+    axis.title = element_text(face = "bold", size = rel(2)), # Changing axis label font
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_text(color = "black", face = "bold", size = rel(2)), # Changing y axis text font
+    legend.text = element_text(size = rel(1.2)), # Changing legend text font
+    legend.title = element_text(face = "bold", size = rel(1.5)) # Changing legend title font
+  ) +
+  labs(x = NULL, y = "Abundance (normalized)", title = "") +
+  ggtitle("Akkermansia\n Abundance") +
+  theme(plot.title = element_text(face = "bold", size = rel(2.5), hjust = 0.5)) 
+
+Akkermansia_bar
+
+
+
+
+
+
+
+
+####  Saving  ####
+Bifidobacterium_bar
+Prevotella_bar
+Lachnospiraceae_bar
+Akkermansia_bar
+
+ggsave(filename="Bifidobacterium_levels.png", plot = Bifidobacterium_bar, width = 7, height = 6)
+ggsave(filename="Prevotella_levels.png", plot = Prevotella_bar, width = 7, height = 6)
+ggsave(filename="Lachnospiraceae_levels????.png", plot = Lachnospiraceae_bar, width = 7, height = 6)
+ggsave(filename="Akkermansia_levels.png", plot = Akkermansia_bar, width = 7, height = 6)
